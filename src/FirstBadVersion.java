e2530efbc505f22903ff863d129ff3df2ed372bc
Shresth Pratap Singh <shresth@MacBook-Air.local>
2020-05-02 23:23:30 +0530

    First Bad Version :  May-01

--- src/FirstBadVersion.java
+++ src/FirstBadVersion.java
@@ -1,6 +1,7 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,63 +9,63 @@ import java.util.Set;
 public class FirstBadVersion{
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.firstBadVersion(5));
+        System.out.println(sol.firstBadVersion(1));
     }
 }
 
 
 class Solution extends VersionControl {
+    int min = Integer.MAX_VALUE;
 
     public int firstBadVersion(int n) {
-        int array[] = getVersionArray(n);
-        return searchVersion(array,Integer.MAX_VALUE,0,n-1);
+        return searchVersion(1,n);
     }
 
-    public int searchVersion(int array[],int min,int l,int u){
+    public int searchVersion(int l,int u){
 
-        int m = l + (u - l)/2 ;
+        int m = l + (u-l)/2 ;
 
-        if (u == l){
-            if (array[l]< min){
-                if (isBadVersion(array[l])){
-                    min = array[l];
-                }
-            }
+        if (u < l) {
             return min;
         }
 
-        if (array[m] < min){
-            if (isBadVersion(array[m])){
-                min = array[m];
-                u = m-1;
+        if (m < min){
+            if (isBadVersion(m)){
+                min = m;
+                return searchVersion(l,m-1);
             }else{
-                l = m+1;
+                return searchVersion(m+1,u);
             }
-        } else{
-            u = m-1;
+        } else if(m>min){
+            return searchVersion(l,m-1);
         }
-        return searchVersion(array,min,l,u);
-    }
-
-    public int[] getVersionArray(int n){
-        int array[] = new int[n];
-
-        for(int i = 1;i<=n;i++){
-            array[i-1] = i;
+        else{
+            return min;
         }
-
-        return array;
     }
 }
 
 class VersionControl{
     public boolean isBadVersion(int n){
-        Set<Integer> set = new HashSet<Integer>();
-        set.add(4);
-        set.add(5);
-        if (set.contains(n)){
+        if (n >= 1){
             return true;
+        }else{
+            return false;
         }
-        return false;
     }
 }
+
+    //SOLUTION:
+//    public int firstBadVersion(int n) {
+//        int left = 1;
+//        int right = n;
+//        while (left < right) {
+//            int mid = left + (right - left) / 2;
+//            if (isBadVersion(mid)) {
+//                right = mid;
+//            } else {
+//                left = mid + 1;
+//            }
+//        }
+//        return left;
+//    }
